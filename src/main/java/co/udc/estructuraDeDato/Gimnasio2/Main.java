ab2039b0b0a85655cce7e590f04356cc2d1f3310
Kevin Manuel Gómez Rojas <kgomezr2@unicartagena.edu.co>
2026-06-02 20:12:10 -0500

    feat: primer commit para main

---
+++ src/main/java/co/udc/estructuraDeDato/Gimnasio2/Main.java
@@ -0,0 +1,8 @@
+package co.udc.estructuraDeDato.Gimnasio2;
+
+public class Main {
+
+    void main(){
+        // Aqui se usaran los servicios proximamente, de momento se sube para pushear main
+    }
+}
